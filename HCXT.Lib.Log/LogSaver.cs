7d5deb1458c55c20166a1594c6d2c3a23730a5c0
wulei1977 <wulei1977@hotmail.com>
2014-11-25 15:29:35 +0800

    1.2012.0912.1304 // 更新日志 // // 2012年09月12日 //     版 本 号：1.0.2.2 //     文件版本：1.2012.0912.1304 //     更新内容：为Log方法和COW方法分别增加一个参数logType为string的重载 // // 2012年08月09日 //     版 本 号：1.0.2.1 //     文件版本：1.2012.0809.1212 //     更新内容：重新封装，为程序集生成强名称的密钥对，允许部分受信任的代码调用具有强名称的程序集 // // 2012年04月23日 //     版 本 号：1.0.1.1 //     文件版本：1.2012.0423.2306 //     更新内容：重新封装，规范了工程名称和命名空间。[HCXT.Lib.Log] // // 2012年02月06日 //     版 本 号：1.0.0.1 //     文件版本：1.2012.0206.1459 //     更新内容：第一个版本的日志操作封装类库。 //               实现了动态指定log4net配置文件及日志保存器功能。 //               实现了输出日志到控制台功能。

---
+++ HCXT.Lib.Log/LogSaver.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.IO;
+using log4net;
+
+namespace HCXT.Lib.Log
+{
+    /// <summary>
+    /// 日志保存器封装类
+    /// </summary>
+    public class LogSaver
+    {
+        /// <summary>日志保存器实例</summary>
+        private ILog _saver = null;
+        /// <summary>日志保存器实例</summary>
+        public ILog Saver
+        {
+            get { return _saver; }
+            set { _saver = value; }
+        }
+
+        /// <summary>log4net的配置文件名</summary>
+        private string _log4netConfigFile;
+        /// <summary>log4net的配置文件名</summary>
+        public string Log4netConfigFile
+        {
+            get { return _log4netConfigFile; }
+            set
+            {
+                _log4netConfigFile = value;
+                FileInfo fi = new FileInfo(_log4netConfigFile);
+                log4net.Config.XmlConfigurator.Configure(fi);
+            }
+        }
+
+        /// <summary>log4net的配置文件名</summary>
+        private string _logSaverName;
+        /// <summary>log4net的配置文件名</summary>
+        public string LogSaverName
+        {
+            get { return _logSaverName; }
+            set
+            {
+                _logSaverName = value;
+                _saver = LogManager.GetLogger(_logSaverName);
+            }
+        }
+
+
+        /// <summary>
+        /// 默认构造方法
+        /// </summary>
+        /// <param name="log4netConfigFile">log4net配置文件名称</param>
+        public LogSaver(string log4netConfigFile)
+        {
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(log4netConfigFile));
+        }
+        /// <summary>
+        /// 构造方法，初始化日志保存器
+        /// </summary>
+        /// <param name="log4netConfigFile">log4net配置文件名称</param>
+        /// <param name="logSaverName">log4net适配器节点名称</param>
+        public LogSaver(string log4netConfigFile,string logSaverName)
+        {
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(log4netConfigFile));
+            _saver = LogManager.GetLogger(logSaverName);
+        }
+
+        /// <summary>
+        /// 输出日志（默认日志类型为：LogType.INFO）
+        /// </summary>
+        /// <param name="logMessage">日志消息体</param>
+        public void Log(string logMessage)
+        {
+            Log(LogType.INFO, logMessage);
+        }
+        /// <summary>
+        /// 输出日志
+        /// </summary>
+        /// <param name="logType">日志类型</param>
+        /// <param name="logMessage">日志消息体</param>
+        public void Log(LogType logType, string logMessage)
+        {
+            if (_saver != null)
+            {
+                switch (logType)
+                {
+                    case LogType.INFO:
+                        _saver.Info(logMessage);
+                        break;
+                    case LogType.DEBUG:
+                        _saver.Debug(logMessage);
+                        break;
+                    case LogType.WARN:
+                        _saver.Warn(logMessage);
+                        break;
+                    case LogType.ERROR:
+                        _saver.Error(logMessage);
+                        break;
+                    case LogType.FATAL:
+                        _saver.Fatal(logMessage);
+                        break;
+                }
+            }
+        }
+        /// <summary>
+        /// 输出日志
+        /// </summary>
+        /// <param name="logType">日志类型</param>
+        /// <param name="logMessage">日志消息体</param>
+        public void Log(string logType, string logMessage)
+        {
+            if (_saver != null)
+            {
+                switch (logType.ToUpper())
+                {
+                    case "INFO":
+                        _saver.Info(logMessage);
+                        break;
+                    case "DEBUG":
+                        _saver.Debug(logMessage);
+                        break;
+                    case "WARN":
+                        _saver.Warn(logMessage);
+                        break;
+                    case "ERROR":
+                        _saver.Error(logMessage);
+                        break;
+                    case "FATAL":
+                        _saver.Fatal(logMessage);
+                        break;
+                    default:
+                        _saver.Info(logMessage);
+                        break;
+                }
+            }
+        }
+        /// <summary>
+        /// 保存Info日志
+        /// </summary>
+        /// <param name="logMessage">日志消息体</param>
+        public void Info(string logMessage)
+        {
+            if (_saver != null)
+                _saver.Info(logMessage);
+        }
+        /// <summary>
+        /// 保存Debug日志
+        /// </summary>
+        /// <param name="logMessage">日志消息体</param>
+        public void Debug(string logMessage)
+        {
+            if (_saver != null)
+                _saver.Debug(logMessage);
+        }
+        /// <summary>
+        /// 保存Warn日志
+        /// </summary>
+        /// <param name="logMessage">日志消息体</param>
+        public void Warn(string logMessage)
+        {
+            if (_saver != null)
+                _saver.Warn(logMessage);
+        }
+        /// <summary>
+        /// 保存Error日志
+        /// </summary>
+        /// <param name="logMessage">日志消息体</param>
+        public void Error(string logMessage)
+        {
+            if (_saver != null)
+                _saver.Error(logMessage);
+        }
+        /// <summary>
+        /// 保存Fatal日志
+        /// </summary>
+        /// <param name="logMessage">日志消息体</param>
+        public void Fatal(string logMessage)
+        {
+            if (_saver != null)
+                _saver.Fatal(logMessage);
+        }
+
+        /// <summary>
+        /// 在控制台界面输出日志信息(Console.Out.WriteLine)（默认日志类型为：LogType.INFO）
+        /// </summary>
+        /// <param name="logMessage">日志消息体</param>
+        public void COW(string logMessage)
+        {
+            COW(LogType.INFO, logMessage);
+        }
+        /// <summary>
+        /// 在控制台界面输出日志信息(Console.Out.WriteLine)
+        /// </summary>
+        /// <param name="logType">日志类型</param>
+        /// <param name="logMessage">日志消息体</param>
+        public void COW(LogType logType, string logMessage)
+        {
+            Console.Out.WriteLine("{0} [{1}] {2}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), logType, logMessage);
+        }
+        /// <summary>
+        /// 在控制台界面输出日志信息(Console.Out.WriteLine)
+        /// </summary>
+        /// <param name="logType">日志类型</param>
+        /// <param name="logMessage">日志消息体</param>
+        public void COW(string logType, string logMessage)
+        {
+            Console.Out.WriteLine("{0} [{1}] {2}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), logType, logMessage);
+        }
+
+        /// <summary>
+        /// 日志类型转换。由LogType枚举类型转换成字符串类型
+        /// </summary>
+        /// <param name="logType">LogType枚举类型</param>
+        /// <returns>字符串日志类型</returns>
+        public static string LogTypeConvert(LogType logType)
+        {
+            string Result;
+            switch (logType)
+            {
+                case LogType.INFO:
+                    Result = "Info";
+                    break;
+                case LogType.DEBUG:
+                    Result = "Debug";
+                    break;
+                case LogType.WARN:
+                    Result = "Warn";
+                    break;
+                case LogType.ERROR:
+                    Result = "Error";
+                    break;
+                case LogType.FATAL:
+                    Result = "Fatal";
+                    break;
+                default:
+                    Result = "Info";
+                    break;
+            }
+            return Result;
+        }
+        /// <summary>
+        /// 日志类型转换。由字符串类型转换成LogType枚举类型
+        /// </summary>
+        /// <param name="logType">字符串日志类型</param>
+        /// <returns>LogType枚举类型</returns>
+        public static LogType LogTypeConvert(string logType)
+        {
+            LogType Result;
+            logType = logType.Trim().ToUpper();
+            switch (logType)
+            {
+                case "INFO":
+                    Result = LogType.INFO;
+                    break;
+                case "DEBUG":
+                    Result = LogType.DEBUG;
+                    break;
+                case "WARN":
+                    Result = LogType.WARN;
+                    break;
+                case "ERROR":
+                    Result = LogType.ERROR;
+                    break;
+                case "FATAL":
+                    Result = LogType.FATAL;
+                    break;
+                default:
+                    Result = LogType.INFO;
+                    break;
+            }
+            return Result;
+        }
+    }
+
+    /// <summary>
+    /// 日志类型枚举
+    /// </summary>
+    public enum LogType
+    {
+        /// <summary>调试日志输出</summary>
+        DEBUG,
+        /// <summary>异常日志输出</summary>
+        ERROR,
+        /// <summary>致命错误日志输出</summary>
+        FATAL,
+        /// <summary>消息日志输出</summary>
+        INFO,
+        /// <summary>告警日志输出</summary>
+        WARN
+    }
+
+    ///<summary>
+    /// 委托：保存日志
+    ///</summary>
+    ///<param name="logType">日志类型 (Info,Debug,Warn,Error,Fatal)</param>
+    ///<param name="logMessage">日志消息体</param>
+    public delegate void DelegateOnLog_Str(string logType, string logMessage);
+
+    ///<summary>
+    /// 委托：保存日志
+    ///</summary>
+    ///<param name="logType">日志类型 (LogType枚举)</param>
+    ///<param name="logMessage">日志消息体</param>
+    public delegate void DelegateOnLog(LogType logType, string logMessage);
+}
---
+++ HCXT.Lib.Log/Properties/AssemblyInfo.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// 有关程序集的常规信息通过下列属性集
+// 控制。更改这些属性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("HCXT.Lib.Log")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HCXT.Lib.Log")]
+[assembly: AssemblyCopyright("版权所有 (C)  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 使此程序集中的类型
+// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
+// 则将该类型上的 ComVisible 属性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("c85bd37c-9fe5-4cf2-9a27-c3e9cb6b5ade")]
+
+// 允许部分受信任的代码调用具有强名称的程序集
+[assembly: AllowPartiallyTrustedCallers()]
+// 使用文件的名称初始化 AssemblyKeyFileAttribute 类的新实例，该文件包含为正在属性化的程序集生成强名称的密钥对。
+[assembly: AssemblyKeyFile(@"E:\Work\华创讯通\HCXT类库\HCXT.Lib.Log\HCXT.Lib.Log\bin\Debug\HCXT.Lib.Log.snk")]
+
+// 程序集的版本信息由下面四个值组成:
+//
+//      主版本
+//      次版本 
+//      内部版本号
+//      修订号
+//
+// 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
+// 方法是按如下所示使用“*”:
+[assembly: AssemblyVersion("1.0.2.2")]
+[assembly: AssemblyFileVersion("1.2012.0912.1304")]
+
+// 更新日志
+//
+// 2012年09月12日
+//     版 本 号：1.0.2.2
+//     文件版本：1.2012.0912.1304
+//     更新内容：为Log方法和COW方法分别增加一个参数logType为string的重载
+//
+// 2012年08月09日
+//     版 本 号：1.0.2.1
+//     文件版本：1.2012.0809.1212
+//     更新内容：重新封装，为程序集生成强名称的密钥对，允许部分受信任的代码调用具有强名称的程序集
+//
+// 2012年04月23日
+//     版 本 号：1.0.1.1
+//     文件版本：1.2012.0423.2306
+//     更新内容：重新封装，规范了工程名称和命名空间。[HCXT.Lib.Log]
+//
+// 2012年02月06日
+//     版 本 号：1.0.0.1
+//     文件版本：1.2012.0206.1459
+//     更新内容：第一个版本的日志操作封装类库。
+//               实现了动态指定log4net配置文件及日志保存器功能。
+//               实现了输出日志到控制台功能。
